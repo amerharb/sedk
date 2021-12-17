1a03a319e16b6312009c3eea0098efc716968ef1
Amer Harb <amer.har@gmail.com>
2021-12-17 14:36:46 +0100

    first attempt for sql builder

---
+++ src/aSql.ts
@@ -0,0 +1,188 @@
+export class ASql {
+  private dbSchema: Table[]
+  private table: Table
+  private columns: Column[]
+  private operationConditions: (OperatorCondition | Group)[] = []
+  private steps: STEPS[] = []
+
+  constructor(tables: Table[]) {
+    this.dbSchema = tables
+  }
+
+  public select(...columns: Column[]): ASql {
+    //TODO check that these columns are part of database
+    this.columns = columns
+    this.steps.push(STEPS.SELECT)
+    return this
+  }
+
+  public from(table: Table): ASql {
+    //TODO check if this table are part of database
+    //TODO: check that last step was SELECT before add FROM step
+    this.table = table
+    this.steps.push(STEPS.FROM)
+    return this
+  }
+
+  public where(condition: Condition): ASql;
+  public where(left: Condition, operator: Operator, right: Condition): ASql;
+  public where(left: Condition, operator?: Operator, right?: Condition): ASql {
+    //TODO: check that last step was FROM before add WHERE step
+    if (!operator || !right) {
+      this.operationConditions.push(new OperatorCondition(null, left))
+    } else if (operator && right) {
+      this.operationConditions.push(Group.Open)
+      this.operationConditions.push(new OperatorCondition(null, left))
+      this.operationConditions.push(new OperatorCondition(operator, right))
+      this.operationConditions.push(Group.Close)
+    }
+    this.steps.push(STEPS.WHERE)
+    return this
+  }
+
+  public and(condition: Condition): ASql {
+    //TODO: check that last step was WHERE or OR before add AND step
+    this.operationConditions.push(new OperatorCondition(Operator.AND, condition))
+    this.steps.push(STEPS.AND)
+    return this
+  }
+
+  public or(condition: Condition): ASql {
+    //TODO: check that last step was WHERE or AND before add OR step
+    this.operationConditions.push(new OperatorCondition(Operator.OR, condition))
+    this.steps.push(STEPS.OR)
+    return this
+  }
+
+  public getSQL(): string {
+    let result = `SELECT ${this.columns.join(', ')} FROM ${this.table}
+        `
+    if (this.operationConditions && this.operationConditions.length > 0) {
+      let i = 0
+      let opCond = this.operationConditions[i]
+      if (opCond instanceof OperatorCondition) {
+        result += `  WHERE ${opCond.getCondition()}` //first cond has no operator
+      } else {
+        if (opCond === Group.Open) {
+          const nextOpCond = this.operationConditions[i + 1]
+          if (nextOpCond instanceof OperatorCondition) {
+            const op = (nextOpCond.getOperator() ?? '').toString()
+            result += ` ${op} ${Group.Open} `
+          }
+        }
+        i++
+        opCond = this.operationConditions[i]
+        if (opCond instanceof OperatorCondition)
+          result += ` ${opCond.getCondition()} `
+        i++
+        opCond = this.operationConditions[i]
+        if (opCond instanceof OperatorCondition)
+          result += ` ${opCond.getOperator()} ${opCond.getCondition()} `
+        i++
+        opCond = this.operationConditions[i]
+        if (opCond === Group.Close)
+          result += ` ${opCond} `
+      }
+      i++
+      while (i < this.operationConditions.length) {
+        opCond = this.operationConditions[i]
+        if (opCond instanceof OperatorCondition) {
+          result += ` ${opCond.getOperator()} ${opCond.getCondition()} `
+        }
+        i++
+      }
+
+    }
+    return result
+  }
+}
+
+export class Column {
+  private readonly columnName: string
+
+  constructor(columnName: string) {
+    this.columnName = columnName
+  }
+
+  public isEqual(value: string): Condition {
+    return new Condition(this, Qualifier.Equal, value)
+  }
+
+  public toString() {
+    return this.columnName
+  }
+}
+
+enum STEPS {
+    SELECT = 'select',
+    FROM = 'from',
+    WHERE = 'where',
+    AND = 'and',
+    OR = 'or',
+}
+
+export class Table {
+  private readonly tableName: string
+  private readonly columns: Column[]
+
+  constructor(tableName: string, columns: Column[]) {
+    this.tableName = tableName
+    this.columns = columns
+  }
+
+  getColumns() {
+    return this.columns
+  }
+
+  public toString() {
+    return this.tableName
+  }
+}
+
+export class Condition {
+  private readonly column: Column
+  private readonly qualifier: Qualifier
+  private readonly value: string
+
+  constructor(column: Column, qualifier: Qualifier, value: string) {
+    this.column = column
+    this.qualifier = qualifier
+    this.value = value
+  }
+
+  public toString() {
+    return `${this.column} ${this.qualifier} ${this.value}`
+  }
+}
+
+export class OperatorCondition {
+  private readonly operator: Operator | null
+  private readonly condition: Condition
+
+  constructor(operator: Operator | null, condition: Condition) {
+    this.operator = operator
+    this.condition = condition
+  }
+
+  public getOperator(): Operator | null {
+    return this.operator
+  }
+
+  public getCondition(): Condition {
+    return this.condition
+  }
+}
+
+enum Qualifier {
+    Equal = '=',
+}
+
+enum Group {
+    Open = '(',
+    Close = ')',
+}
+
+export enum Operator {
+    AND = 'AND',
+    OR = 'OR',
+}

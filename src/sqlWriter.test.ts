6469635d8de54002ece53eb676fd98818d6d1707
Amer Harb <amer.harb@gmail.com>
2021-12-19 21:13:15 +0100

    add unit test for OR

--- src/sqlWriter.test.ts
+++ src/sqlWriter.test.ts
@@ -88,4 +88,41 @@ describe('test from one table', () => {
       .replace(/ +/g, ' ')
     expect(received).toEqual('SELECT col1, col2 FROM testTable WHERE ( col1 = x OR col2 = y ) AND col3 = z')
   })
+
+  it('has correct select 2 columns from one table with where has 1 condition then AND after it without parentheses', () => {
+    const received = asql
+      .select(column1, column2)
+      .from(table)
+      .where(column1.isEqual('x'))
+      .or(column2.isEqual('y'))
+      .getSQL()
+      .replace(/\n/g, ' ')
+      .replace(/ +/g, ' ')
+    expect(received).toEqual('SELECT col1, col2 FROM testTable WHERE col1 = x OR col2 = y')
+  })
+
+  it('has correct select 2 columns from one table with where has 2 conditions then OR after it without parentheses', () => {
+    const received = asql
+      .select(column1, column2)
+      .from(table)
+      .where(column1.isEqual('x'), Operator.AND, column2.isEqual('y'))
+      .or(column3.isEqual('z'))
+      .getSQL()
+      .replace(/\n/g, ' ')
+      .replace(/ +/g, ' ')
+    expect(received).toEqual('SELECT col1, col2 FROM testTable WHERE ( col1 = x AND col2 = y ) OR col3 = z')
+  })
+
+  it('has correct select 2 columns from one table with where has 1 condition then OR after it then AND without parentheses', () => {
+    const received = asql
+      .select(column1, column2)
+      .from(table)
+      .where(column1.isEqual('x'), Operator.AND, column2.isEqual('y'))
+      .and(column3.isEqual('z1'))
+      .or(column3.isEqual('z2'))
+      .getSQL()
+      .replace(/\n/g, ' ')
+      .replace(/ +/g, ' ')
+    expect(received).toEqual('SELECT col1, col2 FROM testTable WHERE ( col1 = x AND col2 = y ) AND col3 = z1 OR col3 = z2')
+  })
 })

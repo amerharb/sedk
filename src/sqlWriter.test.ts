daab26ddf3a1ee454d2fe8a41694a0ea7788d059
Amer Harb <amer.harb@gmail.com>
2021-12-19 20:04:51 +0100

    add more unit test

--- src/sqlWriter.test.ts
+++ src/sqlWriter.test.ts
@@ -1,14 +1,28 @@
 'use strict'
 import * as sql from './sqlWriter'
 
-describe('simple SELECT FROM', () => {
+describe('test from one table', () => {
   // database schema
   const column1 = new sql.Column('col1',)
   const column2 = new sql.Column('col2',)
   const column3 = new sql.Column('col3',)
   const table = new sql.Table('testTable', [column1, column2, column3])
 
-  test('simple SELECT FROM', () => {
+  it('has correct select 1 column from one table', () => {
+
+    const asql = new sql.ASql([table])
+
+    const case1 = asql
+      .select(column1)
+      .from(table)
+      .getSQL()
+      .replace(/\n/g, ' ')
+      .replace(/ +/g, ' ')
+
+    expect(case1).toContain('SELECT col1 FROM testTable')
+  })
+
+  it('has correct select 2 columns from one table', () => {
 
     const asql = new sql.ASql([table])
 
@@ -22,7 +36,7 @@ describe('simple SELECT FROM', () => {
     expect(case1).toContain('SELECT col1, col2 FROM testTable')
   })
 
-  test('simple SELECT FROM WHERE', () => {
+  it('has correct select 2 columns from one table with where has 1 condition', () => {
 
     const asql = new sql.ASql([table])
 

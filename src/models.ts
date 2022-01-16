ea6462a10db209d3d28b67a3e68242c88043c749
Amer Harb <amer.harb@gmail.com>
2022-01-16 11:02:50 +0100

    replace enum quilter with type

--- src/models.ts
+++ src/models.ts
@@ -67,12 +67,12 @@ export class BooleanColumn extends Column implements Condition {
   }
 
   public eq(value: null|BooleanLike): Condition {
-    const qualifier = value === null ? Qualifier.Is : Qualifier.Equal
+    const qualifier = value === null ? NullOperator.Is : BooleanOperator.Equal
     return new Condition(new Expression(this), qualifier, new Expression(value))
   }
 
   public eq$(value: null|boolean): Condition {
-    const qualifier = value === null ? Qualifier.Is : Qualifier.Equal
+    const qualifier = value === null ? NullOperator.Is : BooleanOperator.Equal
     const binder = this.binderStore.add(value)
     return new Condition(new Expression(this), qualifier, new Expression(binder))
   }
@@ -99,27 +99,27 @@ export class NumberColumn extends Column {
   public eq(value1: NumberLike, op: Operator, value2: NumberLike): Condition
   public eq(value1: null|NumberLike, op?: Operator, value2?: NumberLike): Condition {
     if (op === undefined && value2 === undefined) {
-      const qualifier = value1 === null ? Qualifier.Is : Qualifier.Equal
+      const qualifier = value1 === null ? NullOperator.Is : BooleanOperator.Equal
       return new Condition(new Expression(this), qualifier, new Expression(value1))
     } else if (op !== undefined && value2 !== undefined) {
-      return new Condition(new Expression(this), Qualifier.Equal, new Expression(value1, op, value2))
+      return new Condition(new Expression(this), BooleanOperator.Equal, new Expression(value1, op, value2))
     }
     throw new Error('not supported case')
   }
 
   public eq$(value: null|number): Condition {
-    const qualifier = value === null ? Qualifier.Is : Qualifier.Equal
+    const qualifier = value === null ? NullOperator.Is : BooleanOperator.Equal
     const binder = this.binderStore.add(value)
     return new Condition(new Expression(this), qualifier, new Expression(binder))
   }
 
   public gt(value: NumberLike): Condition {
-    return new Condition(new Expression(this), Qualifier.GreaterThan, new Expression(value))
+    return new Condition(new Expression(this), BooleanOperator.GreaterThan, new Expression(value))
   }
 
   public gt$(value: number): Condition {
     const binder = this.binderStore.add(value)
-    return new Condition(new Expression(this), Qualifier.GreaterThan, new Expression(binder))
+    return new Condition(new Expression(this), BooleanOperator.GreaterThan, new Expression(binder))
   }
 }
 
@@ -131,12 +131,12 @@ export class TextColumn extends Column {
   public eq(value: Expression): Condition
   public eq(value: null|string|TextColumn): Condition
   public eq(value: null|string|TextColumn|Expression): Condition {
-    const qualifier = value === null ? Qualifier.Is : Qualifier.Equal
+    const qualifier = value === null ? NullOperator.Is : BooleanOperator.Equal
     return new Condition(new Expression(this), qualifier, new Expression(value))
   }
 
   public eq$(value: null|string): Condition {
-    const qualifier = value === null ? Qualifier.Is : Qualifier.Equal
+    const qualifier = value === null ? NullOperator.Is : BooleanOperator.Equal
     const binder = this.binderStore.add(value)
     return new Condition(new Expression(this), qualifier, new Expression(binder))
   }
@@ -148,10 +148,9 @@ export class TextColumn extends Column {
 
 export class Condition implements Expression {
   public readonly left: Expression
-  public readonly qualifier?: Qualifier
+  public readonly operator?: Qualifier
   public readonly right?: Expression
 
-  public readonly operator?: Operator
   public readonly leftType: ExpressionType
   public readonly rightType: ExpressionType
   public readonly resultType: ExpressionType
@@ -159,46 +158,32 @@ export class Condition implements Expression {
   public readonly notRight: boolean
 
   constructor(left: Expression)
-  constructor(left: Expression, qualifier: Qualifier, right: Expression)
-  constructor(left: Expression, qualifier: Qualifier, right: Expression, notLeft: boolean, notRight: boolean)
-  constructor(left: Expression, qualifier?: Qualifier, right?: Expression, notLeft?: boolean, notRight?: boolean) {
+  constructor(left: Expression, operator: Qualifier, right: Expression)
+  constructor(left: Expression, operator: Qualifier, right: Expression, notLeft: boolean, notRight: boolean)
+  constructor(left: Expression, operator?: Qualifier, right?: Expression, notLeft?: boolean, notRight?: boolean) {
     // TODO: validate if qualifier is valid for the "right" type, for example Greater or Lesser does not work with string
     this.left = left
-    this.qualifier = qualifier
+    this.operator = operator
     this.right = right
 
     this.leftType = left.resultType
     this.resultType = ExpressionType.BOOLEAN
     this.notLeft = getNotValueOrThrow(notLeft, left.resultType)
-    if (qualifier === undefined && right === undefined && left.resultType === ExpressionType.BOOLEAN) {
-      this.operator = undefined
+    if (operator === undefined && right === undefined && left.resultType === ExpressionType.BOOLEAN) {
       this.rightType = ExpressionType.NOT_EXIST
       this.notRight = false
-    } else if (qualifier !== undefined && right !== undefined) {
-      this.operator = Condition.getOperatorFromQualifier(qualifier)
+    } else if (operator !== undefined && right !== undefined) {
       this.rightType = right.resultType
       this.notRight = getNotValueOrThrow(notRight, right.resultType)
     }
   }
 
-  private static getOperatorFromQualifier(qualifier: Qualifier): Operator {
-    switch (qualifier) {
-    case Qualifier.Equal:
-      return BooleanOperator.Equal
-    case Qualifier.Is:
-      return NullOperator.Is
-    case Qualifier.GreaterThan:
-      return BooleanOperator.GreaterThan
-    }
-  }
-
   public toString(): string {
-    if (this.qualifier !== undefined && this.right !== undefined)
-      return `${this.left} ${this.qualifier} ${this.right}`
+    if (this.operator !== undefined && this.right !== undefined)
+      return `${this.left} ${this.operator} ${this.right}`
     else
       return this.left.toString()
   }
-
 }
 
 //TODO: include other value type like date-time
@@ -382,24 +367,6 @@ enum ExpressionType {
   BINDER,
 }
 
-/*
-Remember to redefine everything in Qualifier enum again in LogicalOperator enum.
-currently, we can not read from the same source as we can't have computed string value in enum
-there is an open issue for this: https://github.com/microsoft/TypeScript/issues/40793
- */
-enum Qualifier { //Relational operator
-  Equal = '=',
-  // TODO: add "in" Qualifier
-  // Like = 'like',
-  // In = 'IN',
-  Is = 'IS',
-  // TODO: add other Qualifier for number
-  GreaterThan = '>',
-  // GreaterOrEqual = '>=',
-  // Lesser = '<',
-  // LesserOrEqual = '<=',
-}
-
 // TODO: add other arithmetic operators
 export enum ArithmeticOperator {
   ADD = '+',
@@ -420,6 +387,7 @@ export enum NullOperator {
   Is = 'IS',
 }
 
+export type Qualifier = NullOperator|BooleanOperator
 export type Operator = NullOperator|BooleanOperator|ArithmeticOperator|TextOperator
 
 export type PostgresBinder = {

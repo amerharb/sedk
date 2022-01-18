4b3757d54432d0c0bed53b8d054d529360ed0b2d
Amer Harb <amer.harb@gmail.com>
2022-01-18 08:50:35 +0100

    Refactor 4: separate schema and steps in diffreant files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
-export { Builder, LogicalOperator } from './builder'
+export { Builder } from './builder'
+export { LogicalOperator } from './steps'
 export { e } from './functions'
-export { Database, Table, BooleanColumn, NumberColumn, TextColumn, Operator } from './models'
-export { ColumnNotFoundError, TableNotFoundError, InvalidExpressionError } from './errors'
+export {
+  Operator,
+  ArithmeticOperator,
+  BooleanOperator,
+  TextOperator,
+} from './models'
+export {
+  ColumnNotFoundError,
+  TableNotFoundError,
+  InvalidExpressionError,
+} from './errors'
+export {
+  Database,
+  Table,
+  BooleanColumn,
+  NumberColumn,
+  TextColumn,
+} from './schema'

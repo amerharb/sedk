90cfcdcf06ef9150fb838b95a107f1dffdcbbda5
Amer Harb <amer.harb@gmail.com>
2022-04-11 14:46:23 +0200

    move unit test for LEFT_JOIN step

--- src/option.ts
+++ src/option.ts
@@ -8,7 +8,17 @@ export type BuilderOption = {
   addAsBeforeTableAlias?: 'always'|'never'
 }
 
-const defaultOption: BuilderOption = {
+export type BuilderOptionStrict = {
+  useSemicolonAtTheEnd: boolean
+  addAscAfterOrderByItem: 'always'|'never'|'when mentioned'
+  addNullsLastAfterOrderByItem: 'always'|'never'|'when mentioned'
+  addAsBeforeColumnAlias: 'always'|'never'
+  addPublicSchemaName: 'always'|'never'|'when other schema mentioned'
+  addTableName: 'always'|'when two tables or more'
+  addAsBeforeTableAlias: 'always'|'never'
+}
+
+const defaultOption: BuilderOptionStrict = {
   useSemicolonAtTheEnd: true,
   addAscAfterOrderByItem: 'when mentioned',
   addNullsLastAfterOrderByItem: 'when mentioned',
@@ -19,14 +29,14 @@ const defaultOption: BuilderOption = {
 }
 Object.freeze(defaultOption)
 
-export function fillUndefinedOptionsWithDefault(option: BuilderOption): BuilderOption {
-  const result: BuilderOption = {}
-  result.useSemicolonAtTheEnd = option.useSemicolonAtTheEnd ?? defaultOption.useSemicolonAtTheEnd
-  result.addAscAfterOrderByItem = option.addAscAfterOrderByItem ?? defaultOption.addAscAfterOrderByItem
-  result.addNullsLastAfterOrderByItem = option.addNullsLastAfterOrderByItem ?? defaultOption.addNullsLastAfterOrderByItem
-  result.addAsBeforeColumnAlias = option.addAsBeforeColumnAlias ?? defaultOption.addAsBeforeColumnAlias
-  result.addPublicSchemaName = option.addPublicSchemaName ?? defaultOption.addPublicSchemaName
-  result.addTableName = option.addTableName ?? defaultOption.addTableName
-  result.addAsBeforeTableAlias = option.addAsBeforeTableAlias ?? defaultOption.addAsBeforeTableAlias
-  return result
+export function fillUndefinedOptionsWithDefault(option: BuilderOption): BuilderOptionStrict {
+  return {
+    useSemicolonAtTheEnd: option.useSemicolonAtTheEnd ?? defaultOption.useSemicolonAtTheEnd,
+    addAscAfterOrderByItem: option.addAscAfterOrderByItem ?? defaultOption.addAscAfterOrderByItem,
+    addNullsLastAfterOrderByItem: option.addNullsLastAfterOrderByItem ?? defaultOption.addNullsLastAfterOrderByItem,
+    addAsBeforeColumnAlias: option.addAsBeforeColumnAlias ?? defaultOption.addAsBeforeColumnAlias,
+    addPublicSchemaName: option.addPublicSchemaName ?? defaultOption.addPublicSchemaName,
+    addTableName: option.addTableName ?? defaultOption.addTableName,
+    addAsBeforeTableAlias: option.addAsBeforeTableAlias ?? defaultOption.addAsBeforeTableAlias,
+  }
 }
--- test/leftJoinStep.test.ts
+++ test/leftJoinStep.test.ts
@@ -3,12 +3,14 @@ import { database } from './database'
 //Alias
 const table1 = database.s.public.t.table1
 const table1Col1 = database.s.public.t.table1.c.col1
+const table1Col2 = database.s.public.t.table1.c.col2
 const table2 = database.s.public.t.table2
+const table2Col1 = database.s.public.t.table2.c.col1
 
 describe('Test LEFT JOIN Step', () => {
   const sql = new Builder(database)
   afterEach(() => { sql.cleanUp() })
-  describe('basic left join', () => {
+  describe('Basic left join', () => {
     it('Produces [SELECT * FROM "table1" LEFT JOIN "table2" ON "table1"."col1" = "table2"."col1";]', () => {
       const actual = sql
         .selectAsteriskFrom(table1)
@@ -38,5 +40,63 @@ describe('Test LEFT JOIN Step', () => {
 
       expect(actual).toEqual('SELECT * FROM "table1" LEFT JOIN "table2" ON "table1"."col1" = "table2"."col1" OR "table1"."col2" = "table2"."col2";')
     })
+  })
+
+  describe('Different steps after left join', () => {
+    it('Produces [SELECT * FROM "table1" LEFT JOIN "table2" ON "table1"."col1" = "table2"."col1" WHERE "table1"."col1" = \'a\';;]', () => {
+      const actual = sql
+        .selectAsteriskFrom(table1)
+        .leftJoin(table2)
+        .on(table1Col1.eq(table2Col1))
+        .where(table1Col1.eq('a'))
+        .getSQL()
+
+      expect(actual).toEqual('SELECT * FROM "table1" LEFT JOIN "table2" ON "table1"."col1" = "table2"."col1" WHERE "table1"."col1" = \'a\';')
+    })
+
+    it('Produces [SELECT * FROM "table1" LEFT JOIN "table2" GROUP BY "table1"."col2";]', () => {
+      const actual = sql
+        .selectAsteriskFrom(table1)
+        .leftJoin(table2)
+        .on(table1Col1.eq(table2Col1))
+        .groupBy(table1Col2)
+        .getSQL()
+
+      expect(actual).toEqual('SELECT * FROM "table1" LEFT JOIN "table2" ON "table1"."col1" = "table2"."col1" GROUP BY "table1"."col2";')
+    })
+
+    it('Produces [SELECT * FROM "table1" LEFT JOIN "table2" ORDER BY "table1"."col2";]', () => {
+      const actual = sql
+        .selectAsteriskFrom(table1)
+        .leftJoin(table2)
+        .on(table1Col1.eq(table2Col1))
+        .orderBy(table1Col2)
+        .getSQL()
+
+      expect(actual).toEqual('SELECT * FROM "table1" LEFT JOIN "table2" ON "table1"."col1" = "table2"."col1" ORDER BY "table1"."col2";')
+    })
+
+    it('Produces [SELECT * FROM "table1" LEFT JOIN "table2" LIMIT 10;]', () => {
+      const actual = sql
+        .selectAsteriskFrom(table1)
+        .leftJoin(table2)
+        .on(table1Col1.eq(table2Col1))
+        .limit(10)
+        .getSQL()
+
+      expect(actual).toEqual('SELECT * FROM "table1" LEFT JOIN "table2" ON "table1"."col1" = "table2"."col1" LIMIT 10;')
+    })
+
+    it('Produces [SELECT * FROM "table1" LEFT JOIN "table2" OFFSET 20;]', () => {
+      const actual = sql
+        .selectAsteriskFrom(table1)
+        .leftJoin(table2)
+        .on(table1Col1.eq(table2Col1))
+        .offset(20)
+        .getSQL()
+
+      expect(actual).toEqual('SELECT * FROM "table1" LEFT JOIN "table2" ON "table1"."col1" = "table2"."col1" OFFSET 20;')
+    })
+
   })
 })

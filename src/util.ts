cde22fe0286f70ccaecbabca45b4a07de44b1c31
Amer Harb <amer.harb@gmail.com>
2022-04-27 09:50:39 +0200

    refactor

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,12 @@
+import { Step } from './steps/Step'
+
 export function escapeDoubleQuote(source:string): string {
   return source.replace(/"/g, '""')
+}
+
+export function returnStepOrThrow(step: Step|undefined): Step {
+  if (step === undefined) {
+    throw new Error('Step property in builder data is not initialized')
+  }
+  return step
 }

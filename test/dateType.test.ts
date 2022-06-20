e5d075db84f4a895dbdc9e4d03d10f11659e02ad
Amer Harb <amer.harb@gmail.com>
2022-06-20 23:12:10 +0200

    more unit test

--- test/dateType.test.ts
+++ test/dateType.test.ts
@@ -4,17 +4,26 @@ import { database } from './database'
 //Alias
 const table = database.s.public.t.table1
 const col9 = database.s.public.t.table1.c.col9
+const col10 = database.s.public.t.table1.c.col10
 
 describe(`test Date column`, () => {
   const sql = new Builder(database)
   afterEach(() => { sql.cleanUp() })
-  const DATE_2022_06_20_EPOCH = Date.UTC(2022,5, 20)
-  describe(`date equal Date object`, () => {
+  const EPOCH_2022_06_20 = Date.UTC(2022,5, 20)
+  describe(`Equal operator`, () => {
+    it(`Produces [SELECT * FROM "table1" WHERE "col9" = "col10";]`, () => {
+      const actual = sql
+        .selectAsteriskFrom(table)
+        .where(col9.eq(col10))
+        .getSQL()
+
+      expect(actual).toEqual(`SELECT * FROM "table1" WHERE "col9" = "col10";`)
+    })
     it(`Produces [SELECT "col9" FROM "table1" WHERE "col9" = '2022-06-20T00:00:00.000Z';]`, () => {
       const actual = sql
         .select(col9)
         .from(table)
-        .where(col9.eq(new Date(DATE_2022_06_20_EPOCH)))
+        .where(col9.eq(new Date(EPOCH_2022_06_20)))
         .getSQL()
 
       expect(actual).toEqual(`SELECT "col9" FROM "table1" WHERE "col9" = '2022-06-20T00:00:00.000Z';`)
@@ -23,22 +32,30 @@ describe(`test Date column`, () => {
       const actual = sql
         .select(col9)
         .from(table)
-        .where(col9.eq$(new Date(DATE_2022_06_20_EPOCH)))
+        .where(col9.eq$(new Date(EPOCH_2022_06_20)))
 
       const expected = {
         sql: `SELECT "col9" FROM "table1" WHERE "col9" = $1;`,
-        values: [new Date(DATE_2022_06_20_EPOCH)],
+        values: [new Date(EPOCH_2022_06_20)],
       }
       expect(actual.getSQL()).toEqual(expected.sql)
       expect(actual.getBindValues()).toEqual(expected.values)
     })
   })
-  describe(`date not equal Date object`, () => {
+  describe(`Not equal operator`, () => {
+    it(`Produces [SELECT * FROM "table1" WHERE "col9" <>> "col10";]`, () => {
+      const actual = sql
+        .selectAsteriskFrom(table)
+        .where(col9.ne(col10))
+        .getSQL()
+
+      expect(actual).toEqual(`SELECT * FROM "table1" WHERE "col9" <> "col10";`)
+    })
     it(`Produces [SELECT "col9" FROM "table1" WHERE "col9" <> '2022-06-20T00:00:00.000Z';]`, () => {
       const actual = sql
         .select(col9)
         .from(table)
-        .where(col9.ne(new Date(DATE_2022_06_20_EPOCH)))
+        .where(col9.ne(new Date(EPOCH_2022_06_20)))
         .getSQL()
 
       expect(actual).toEqual(`SELECT "col9" FROM "table1" WHERE "col9" <> '2022-06-20T00:00:00.000Z';`)
@@ -47,11 +64,11 @@ describe(`test Date column`, () => {
       const actual = sql
         .select(col9)
         .from(table)
-        .where(col9.ne$(new Date(DATE_2022_06_20_EPOCH)))
+        .where(col9.ne$(new Date(EPOCH_2022_06_20)))
 
       const expected = {
         sql: `SELECT "col9" FROM "table1" WHERE "col9" <> $1;`,
-        values: [new Date(DATE_2022_06_20_EPOCH)],
+        values: [new Date(EPOCH_2022_06_20)],
       }
       expect(actual.getSQL()).toEqual(expected.sql)
       expect(actual.getBindValues()).toEqual(expected.values)

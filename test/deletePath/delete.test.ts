130cad378bd21c22fef8cfa25e78c0f2f26dd7cc
Amer Harb <amer.harb@gmail.com>
2022-07-01 05:06:01 +0200

    add unit test

--- test/deletePath/delete.test.ts
+++ test/deletePath/delete.test.ts
@@ -1,12 +1,15 @@
-import { Builder } from '../../src'
+import * as sedk from '../../src'
 import { database } from '../database'
 
 //Alias
+const AND = sedk.LogicalOperator.AND
+const OR = sedk.LogicalOperator.OR
 const table1 = database.s.public.t.table1
 const table2 = database.s.public.t.table2
 
+
 describe('DELETE Path', () => {
-  const sql = new Builder(database, { throwErrorIfDeleteHasNoCondition: false })
+  const sql = new sedk.Builder(database, { throwErrorIfDeleteHasNoCondition: false })
   afterEach(() => { sql.cleanUp() })
   describe('basic delete all', () => {
     it('Produces [DELETE FROM "table1"]', () => {
@@ -28,6 +31,105 @@ describe('DELETE Path', () => {
       const actual = sql.deleteFrom(table2.as('t2')).getSQL()
 
       expect(actual).toEqual('DELETE FROM "table2" AS "t2";')
+    })
+  })
+  describe('delete with where', () => {
+    it(`Produces [DELETE FROM "table1" WHERE "col1" = 'A';]`, () => {
+      const actual = sql.deleteFrom(table1).where(table1.c.col1.eq('A')).getSQL()
+
+      expect(actual).toEqual('DELETE FROM "table1" WHERE "col1" = \'A\';')
+    })
+    it(`Produces [DELETE FROM "table1" WHERE ( "col1" = 'A' AND "col2" = 'B' );]`, () => {
+      const actual = sql
+        .delete()
+        .from(table1)
+        .where(table1.c.col1.eq('A'), AND , table1.c.col2.eq('B'))
+        .getSQL()
+
+      expect(actual).toEqual(`DELETE FROM "table1" WHERE ( "col1" = 'A' AND "col2" = 'B' );`)
+    })
+    it(`Produces [DELETE FROM "table1" WHERE ( "col1" = 'A' OR "col2" = 'B' );]`, () => {
+      const actual = sql
+        .delete()
+        .from(table1)
+        .where(table1.c.col1.eq('A'), OR, table1.c.col2.eq('B'))
+        .getSQL()
+
+      expect(actual).toEqual(`DELETE FROM "table1" WHERE ( "col1" = 'A' OR "col2" = 'B' );`)
+    })
+    it(`Produces [DELETE FROM "table1" WHERE ( "col1" = 'A' AND "col2" = 'B' OR "col3" = 'C' );]`, () => {
+      const actual = sql
+        .delete()
+        .from(table1)
+        .where(
+          table1.c.col1.eq('A'),
+          AND,
+          table1.c.col2.eq('B'),
+          OR,
+          table1.c.col3.eq('C')
+        )
+        .getSQL()
+
+      expect(actual).toEqual(`DELETE FROM "table1" WHERE ( "col1" = 'A' AND "col2" = 'B' OR "col3" = 'C' );`)
+    })
+    it(`Produces [DELETE FROM "table1" WHERE ( "col1" = 'A' OR "col2" = 'B' AND "col3" = 'C' );]`, () => {
+      const actual = sql
+        .delete()
+        .from(table1)
+        .where(
+          table1.c.col1.eq('A'),
+          OR,
+          table1.c.col2.eq('B'),
+          AND,
+          table1.c.col3.eq('C')
+        )
+        .getSQL()
+
+      expect(actual).toEqual(`DELETE FROM "table1" WHERE ( "col1" = 'A' OR "col2" = 'B' AND "col3" = 'C' );`)
+    })
+  })
+  describe('delete with and/or', () => {
+    it(`Produces [DELETE FROM "table1" WHERE "col1" = 'A' AND "col2" = 'B';]`, () => {
+      const actual = sql
+        .delete()
+        .from(table1)
+        .where(table1.c.col1.eq('A'))
+        .and(table1.c.col2.eq('B'))
+        .getSQL()
+
+      expect(actual).toEqual('DELETE FROM "table1" WHERE "col1" = \'A\' AND "col2" = \'B\';')
+    })
+    it(`Produces [DELETE FROM "table1" WHERE "col1" = 'A' OR "col2" = 'B';]`, () => {
+      const actual = sql
+        .delete()
+        .from(table1)
+        .where(table1.c.col1.eq('A'))
+        .or(table1.c.col2.eq('B'))
+        .getSQL()
+
+      expect(actual).toEqual('DELETE FROM "table1" WHERE "col1" = \'A\' OR "col2" = \'B\';')
+    })
+    it(`Produces [DELETE FROM "table1" WHERE "col1" = 'A' AND "col2" = 'B' OR "col3" = 'C';]`, () => {
+      const actual = sql
+        .delete()
+        .from(table1)
+        .where(table1.c.col1.eq('A'))
+        .and(table1.c.col2.eq('B'))
+        .or(table1.c.col3.eq('C'))
+        .getSQL()
+
+      expect(actual).toEqual(`DELETE FROM "table1" WHERE "col1" = 'A' AND "col2" = 'B' OR "col3" = 'C';`)
+    })
+    it(`Produces [DELETE FROM "table1" WHERE "col1" = 'A' OR "col2" = 'B' AND "col3" = 'C';]`, () => {
+      const actual = sql
+        .delete()
+        .from(table1)
+        .where(table1.c.col1.eq('A'))
+        .or(table1.c.col2.eq('B'))
+        .and(table1.c.col3.eq('C'))
+        .getSQL()
+
+      expect(actual).toEqual(`DELETE FROM "table1" WHERE "col1" = 'A' OR "col2" = 'B' AND "col3" = 'C';`)
     })
   })
 })

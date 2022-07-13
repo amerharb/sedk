d869b75349278a3c2b069d2a6e34b33163abbae7
Amer Harb <amer.harb@gmail.com>
2022-07-13 09:34:28 +0200

    format

--- test/error.test.ts
+++ test/error.test.ts
@@ -34,59 +34,143 @@ describe('Throw desired Errors', () => {
   const sql = new Builder(database)
   afterEach(() => { sql.cleanUp() })
 
-  it('Throws error when 2 WHERE steps added', () => {
-    function actual() {
-      const fromStep = sql.select(col1).from(table1)
+  describe('Error: MoreThanOneWhereStepError', () => {
+    it('Throws error when 2 WHERE steps added', () => {
+      function actual() {
+        const fromStep = sql.select(col1).from(table1)
 
-      // first Where Step
-      fromStep.where(col1.eq('x1'))
-      // second Where Step, should throw
-      fromStep.where(col1.eq('x2'))
-    }
+        // first Where Step
+        fromStep.where(col1.eq('x1'))
+        // second Where Step, should throw
+        fromStep.where(col1.eq('x2'))
+      }
 
-    expect(actual).toThrowError('WHERE step already specified')
-    expect(actual).toThrowError(MoreThanOneWhereStepError)
+      expect(actual).toThrowError('WHERE step already specified')
+      expect(actual).toThrowError(MoreThanOneWhereStepError)
+    })
+    it('Throws error when 2 WHERE steps added after ON step', () => {
+      function actual() {
+        const fromStep = sql
+          .select(col1)
+          .from(table1)
+          .leftJoin(table2)
+          .on(col1.eq(table2.c.col1))
+
+        // first Where Step
+        fromStep.where(col3.eq('x1'))
+        // second Where Step, should throw
+        fromStep.where(col3.eq('x2'))
+      }
+
+      expect(actual).toThrowError('WHERE step already specified')
+      expect(actual).toThrowError(MoreThanOneWhereStepError)
+    })
+    it('Throws error when 2 WHERE steps added after delete', () => {
+      function actual() {
+        const fromStep = sql.deleteFrom(table1)
+
+        // first Where Step
+        fromStep.where(col1.eq('x1'))
+        // second Where Step, should throw
+        fromStep.where(col1.eq('x2'))
+      }
+
+      expect(actual).toThrowError('WHERE step already specified')
+      expect(actual).toThrowError(MoreThanOneWhereStepError)
+    })
   })
 
-  it('Throws error when 2 WHERE steps added after ON step', () => {
-    function actual() {
-      const fromStep = sql
-        .select(col1)
-        .from(table1)
-        .leftJoin(table2)
-        .on(col1.eq(table2.c.col1))
+  describe('Error: InvalidExpressionError', () => {
+    it('Throws error when add invalid operator', () => {
+      function actual() {
+        sql.select(e(1, GT, 'f'))
+      }
 
-      // first Where Step
-      fromStep.where(col3.eq('x1'))
-      // second Where Step, should throw
-      fromStep.where(col3.eq('x2'))
-    }
+      expect(actual).toThrowError('You can not have "NUMBER" and "TEXT" with operator ">"')
+      expect(actual).toThrowError(InvalidExpressionError)
+    })
+    it('Throws error if number added to text', () => {
+      function actual() {
+        sql.select(e(1, ADD, 'a')).getSQL()
+      }
 
-    expect(actual).toThrowError('WHERE step already specified')
-    expect(actual).toThrowError(MoreThanOneWhereStepError)
+      expect(actual).toThrowError('You can not have "NUMBER" and "TEXT" with operator "+"')
+      expect(actual).toThrowError(InvalidExpressionError)
+    })
   })
 
-  it('Throws error when 2 WHERE steps added after delete', () => {
-    function actual() {
-      const fromStep = sql.deleteFrom(table1)
+  describe('Error: MoreThanOneDistinctOrAllError', () => {
+    it('Throws error when more than one DISTINCT passed', () => {
+      function actual() {
+        // @ts-ignore
+        sql.select(DISTINCT, DISTINCT, col1).from(table1)
+      }
 
-      // first Where Step
-      fromStep.where(col1.eq('x1'))
-      // second Where Step, should throw
-      fromStep.where(col1.eq('x2'))
-    }
+      expect(actual).toThrow(/^You can not have more than one DISTINCT or ALL$/)
+      expect(actual).toThrow(MoreThanOneDistinctOrAllError)
+    })
+    it('Throws error when more than one ALL passed', () => {
+      function actual() {
+        // @ts-ignore
+        sql.select(ALL, ALL, col1).from(table1)
+      }
 
-    expect(actual).toThrowError('WHERE step already specified')
-    expect(actual).toThrowError(MoreThanOneWhereStepError)
+      expect(actual).toThrow(/^You can not have more than one DISTINCT or ALL$/)
+      expect(actual).toThrow(MoreThanOneDistinctOrAllError)
+    })
+    it('Throws error when DISTINCT and ALL passed', () => {
+      // @ts-ignore
+      function actual1() { sql.select(ALL, col1, DISTINCT).from(table1) }
+
+      // @ts-ignore
+      function actual2() { sql.select(ALL, DISTINCT, col1).from(table1) }
+
+      // @ts-ignore
+      function actual3() { sql.select(DISTINCT, ALL, col1).from(table1) }
+
+      // @ts-ignore
+      function actual4() { sql.select(DISTINCT, col1, ALL).from(table1) }
+
+      [actual1, actual2, actual3, actual4].forEach(actual => {
+        expect(actual).toThrow(/^You can not have more than one DISTINCT or ALL$/)
+        expect(actual).toThrow(MoreThanOneDistinctOrAllError)
+      })
+    })
   })
 
-  it('Throws error when add invalid operator', () => {
-    function actual() {
-      sql.select(e(1, GT, 'f'))
-    }
+  describe('Error: InvalidConditionError', () => {
+    it(`Throws error when condition created with "NUMBER" "=" "TEXT"`, () => {
+      function actual() {
+        sql
+          .selectAsteriskFrom(table1)
+          .where(col4.bitwiseXor(1).eq('A'))
+          .getSQL()
+      }
 
-    expect(actual).toThrowError('You can not have "NUMBER" and "TEXT" with operator ">"')
-    expect(actual).toThrowError(InvalidExpressionError)
+      expect(actual).toThrow(InvalidConditionError)
+      expect(actual).toThrow(`Condition can not created with "NUMBER" "=" "TEXT"`)
+    })
+    it(`Throws error when condition created with "NUMBER" "=" "DATE"`, () => {
+      function actual() {
+        sql
+          .selectAsteriskFrom(table1)
+          .where(col4.bitwiseXor(1).eq(new Date()))
+          .getSQL()
+      }
+
+      expect(actual).toThrow(InvalidConditionError)
+      expect(actual).toThrow(`Condition can not created with "NUMBER" "=" "DATE"`)
+    })
+    it(`Throws no error when condition created with "NUMBER" "=" "NUMBER"`, () => {
+      function actual() {
+        sql
+          .selectAsteriskFrom(table1)
+          .where(col4.bitwiseXor(1).eq(1))
+          .getSQL()
+      }
+
+      expect(actual).not.toThrow()
+    })
   })
 
   it('Throws error when column not exist', () => {
@@ -111,60 +195,12 @@ describe('Throw desired Errors', () => {
     expect(actual).toThrowError(TableNotFoundError)
   })
 
-  it('Throws error if number added to text', () => {
-    function actual() {
-      sql.select(e(1, ADD, 'a')).getSQL()
-    }
-
-    expect(actual).toThrowError('You can not have "NUMBER" and "TEXT" with operator "+"')
-    expect(actual).toThrowError(InvalidExpressionError)
-  })
-
   it('Throws error when no param to select passed after DISTINCT', () => {
     function actual() {
       sql.select(DISTINCT).from(table1)
     }
 
     expect(actual).toThrow(/^Select step must have at least one parameter after DISTINCT$/)
-  })
-
-  it('Throws error when more than one DISTINCT passed', () => {
-    function actual() {
-      // @ts-ignore
-      sql.select(DISTINCT, DISTINCT, col1).from(table1)
-    }
-
-    expect(actual).toThrow(/^You can not have more than one DISTINCT or ALL$/)
-    expect(actual).toThrow(MoreThanOneDistinctOrAllError)
-  })
-
-  it('Throws error when more than one ALL passed', () => {
-    function actual() {
-      // @ts-ignore
-      sql.select(ALL, ALL, col1).from(table1)
-    }
-
-    expect(actual).toThrow(/^You can not have more than one DISTINCT or ALL$/)
-    expect(actual).toThrow(MoreThanOneDistinctOrAllError)
-  })
-
-  it('Throws error when DISTINCT and ALL passed', () => {
-    // @ts-ignore
-    function actual1() { sql.select(ALL, col1, DISTINCT).from(table1) }
-
-    // @ts-ignore
-    function actual2() { sql.select(ALL, DISTINCT, col1).from(table1) }
-
-    // @ts-ignore
-    function actual3() { sql.select(DISTINCT, ALL, col1).from(table1) }
-
-    // @ts-ignore
-    function actual4() { sql.select(DISTINCT, col1, ALL).from(table1) }
-
-    [actual1, actual2, actual3, actual4].forEach(actual => {
-      expect(actual).toThrow(/^You can not have more than one DISTINCT or ALL$/)
-      expect(actual).toThrow(MoreThanOneDistinctOrAllError)
-    })
   })
 
   it('Throws error when ORDER BY has no param', () => {
@@ -250,40 +286,5 @@ describe('Throw desired Errors', () => {
     }
 
     expect(actual).toThrow(/^Expression Type must be number in aggregate function$/)
-  })
-
-  describe('Error: InvalidConditionError', () => {
-    it(`Throws error when condition created with "NUMBER" "=" "TEXT"`, () => {
-      function actual() {
-        sql
-          .selectAsteriskFrom(table1)
-          .where(col4.bitwiseXor(1).eq('A'))
-          .getSQL()
-      }
-
-      expect(actual).toThrow(InvalidConditionError)
-      expect(actual).toThrow(`Condition can not created with "NUMBER" "=" "TEXT"`)
-    })
-    it(`Throws error when condition created with "NUMBER" "=" "DATE"`, () => {
-      function actual() {
-        sql
-          .selectAsteriskFrom(table1)
-          .where(col4.bitwiseXor(1).eq(new Date()))
-          .getSQL()
-      }
-
-      expect(actual).toThrow(InvalidConditionError)
-      expect(actual).toThrow(`Condition can not created with "NUMBER" "=" "DATE"`)
-    })
-    it(`Throws no error when condition created with "NUMBER" "=" "NUMBER"`, () => {
-      function actual() {
-        sql
-          .selectAsteriskFrom(table1)
-          .where(col4.bitwiseXor(1).eq(1))
-          .getSQL()
-      }
-
-      expect(actual).not.toThrow()
-    })
   })
 })

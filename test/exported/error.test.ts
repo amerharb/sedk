877d1cb3a65b5973f5c355d17b4ced7ac72f545a
Amer Harb <amer.harb@gmail.com>
2022-07-16 09:44:55 +0200

    format

--- test/exported/error.test.ts
+++ test/exported/error.test.ts
@@ -310,11 +310,15 @@ describe('Throw desired Errors', () => {
     expect(actual).toThrow(/^Expression Type must be number in aggregate function$/)
   })
 
-  it(`Throws error "Aggregate function x cannot be used in RETURNING clause"`, () => {
-    function actual() {
-      sql.deleteFrom(table1).returning(f.sum(table1.c.col4).as('whatEver')).getSQL()
-    }
+  describe('Delete Path Errors', () => {
+    const deleteSql = new Builder(database, { throwErrorIfDeleteHasNoCondition: false })
+    afterEach(() => { deleteSql.cleanUp() })
+    it(`Throws error "Aggregate function SUM cannot be used in RETURNING clause"`, () => {
+      function actual() {
+        deleteSql.deleteFrom(table1).returning(f.sum(table1.c.col4).as('whatEver')).getSQL()
+      }
 
-    expect(actual).toThrow(/^Aggregate function SUM cannot be used in RETURNING clause$/)
+      expect(actual).toThrow(/^Aggregate function SUM cannot be used in RETURNING clause$/)
+    })
   })
 })

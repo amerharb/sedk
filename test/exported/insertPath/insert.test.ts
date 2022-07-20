4bb504e49468c02c6502d95ebffb58c6f6a0e882
Amer Harb <amer.harb@gmail.com>
2022-07-20 13:33:21 +0200

    insert with specific columns

--- test/exported/insertPath/insert.test.ts
+++ test/exported/insertPath/insert.test.ts
@@ -3,6 +3,7 @@ import { database } from '../../database'
 
 //Alias
 const table1 = database.s.public.t.table1
+const col1 = table1.c.col1
 
 describe('INSERT Path', () => {
   const sql = new sedk.Builder(database)
@@ -10,12 +11,16 @@ describe('INSERT Path', () => {
   const EPOCH_2022_07_20 = Date.UTC(2022, 6, 20)
   describe('Basic insert all', () => {
     it(`Produces [INSERT INTO "table1" VALUES('A', 1, TRUE, '2022-07-20T00:00:00.000Z');]`, () => {
-      const actual = sql.insertInto(table1).values('A', 1, true, new Date(EPOCH_2022_07_20)).getSQL()
+      const actual = sql.insert().into(table1).values('A', 1, true, new Date(EPOCH_2022_07_20)).getSQL()
       expect(actual).toEqual(`INSERT INTO "table1" VALUES('A', 1, TRUE, '2022-07-20T00:00:00.000Z');`)
     })
     it(`Produces [INSERT INTO "table1" VALUES('B', 2, FALSE, '2022-07-20T00:00:00.000Z');]`, () => {
-      const actual = sql.insert().into(table1).values('B', 2, false, new Date(EPOCH_2022_07_20)).getSQL()
+      const actual = sql.insertInto(table1).values('B', 2, false, new Date(EPOCH_2022_07_20)).getSQL()
       expect(actual).toEqual(`INSERT INTO "table1" VALUES('B', 2, FALSE, '2022-07-20T00:00:00.000Z');`)
+    })
+    it(`Produces [INSERT INTO "table1"("col1") VALUES('A');]`, () => {
+      const actual = sql.insertInto(table1, col1).values('A').getSQL()
+      expect(actual).toEqual(`INSERT INTO "table1"("col1") VALUES('A');`)
     })
   })
 })

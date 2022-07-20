9120c7d8096d78fcf0eb31f5dab64047aaefb3bd
Amer Harb <amer.harb@gmail.com>
2022-07-20 13:51:12 +0200

    improve insert unit test

--- test/exported/insertPath/insert.test.ts
+++ test/exported/insertPath/insert.test.ts
@@ -18,9 +18,15 @@ describe('INSERT Path', () => {
       const actual = sql.insertInto(table1).values(null, 'B', 2, false, new Date(EPOCH_2022_07_20)).getSQL()
       expect(actual).toEqual(`INSERT INTO "table1" VALUES(NULL, 'B', 2, FALSE, '2022-07-20T00:00:00.000Z');`)
     })
+  })
+  describe('Insert specific column', () => {
     it(`Produces [INSERT INTO "table1"("col1") VALUES('A');]`, () => {
       const actual = sql.insertInto(table1, col1).values('A').getSQL()
       expect(actual).toEqual(`INSERT INTO "table1"("col1") VALUES('A');`)
+    })
+    it(`Produces [INSERT INTO "table1"("col1", "col4", "col7") VALUES('A', 1, TRUE);]`, () => {
+      const actual = sql.insertInto(table1, col1).values('A', 1, true).getSQL()
+      expect(actual).toEqual(`INSERT INTO "table1"("col1") VALUES('A', 1, TRUE);`)
     })
   })
 })

fd61874c2982b7868aaa7617d89730507314756b
Amer Harb <amer.harb@gmail.com>
2022-07-20 13:20:29 +0200

    format

--- src/util.ts
+++ src/util.ts
@@ -4,7 +4,7 @@ export function escapeDoubleQuote(source: string): string {
   return source.replace(/"/g, '""')
 }
 
-export function escapeSingleQuote(source: string): string {
+function escapeSingleQuote(source: string): string {
   return source.replace(/'/g, "''")
 }
 
@@ -15,18 +15,18 @@ export function returnStepOrThrow(step: Step|undefined): Step {
   return step
 }
 
-export function getStmtBoolean(value: boolean): string {
-  return value ? 'TRUE' : 'FALSE'
-}
-
-export function getStmtDate(value: Date): string {
-  return `'${escapeSingleQuote(value.toISOString())}'`
-}
-
 export function getStmtNull(): string {
   return 'NULL'
 }
 
+export function getStmtBoolean(value: boolean): string {
+  return value ? 'TRUE' : 'FALSE'
+}
+
 export function getStmtString(value: string): string {
   return `'${escapeSingleQuote(value)}'`
+}
+
+export function getStmtDate(value: Date): string {
+  return `'${escapeSingleQuote(value.toISOString())}'`
 }

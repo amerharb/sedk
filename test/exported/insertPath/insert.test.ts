e37a8625ef534a1e973657d2675f33d1da88fe45
Amer Harb <amer.harb@gmail.com>
2022-07-20 13:44:08 +0200

    add null to test

--- test/exported/insertPath/insert.test.ts
+++ test/exported/insertPath/insert.test.ts
@@ -14,9 +14,9 @@ describe('INSERT Path', () => {
       const actual = sql.insert().into(table1).values('A', 1, true, new Date(EPOCH_2022_07_20)).getSQL()
       expect(actual).toEqual(`INSERT INTO "table1" VALUES('A', 1, TRUE, '2022-07-20T00:00:00.000Z');`)
     })
-    it(`Produces [INSERT INTO "table1" VALUES('B', 2, FALSE, '2022-07-20T00:00:00.000Z');]`, () => {
-      const actual = sql.insertInto(table1).values('B', 2, false, new Date(EPOCH_2022_07_20)).getSQL()
-      expect(actual).toEqual(`INSERT INTO "table1" VALUES('B', 2, FALSE, '2022-07-20T00:00:00.000Z');`)
+    it(`Produces [INSERT INTO "table1" VALUES(NULL, 'B', 2, FALSE, '2022-07-20T00:00:00.000Z');]`, () => {
+      const actual = sql.insertInto(table1).values(null, 'B', 2, false, new Date(EPOCH_2022_07_20)).getSQL()
+      expect(actual).toEqual(`INSERT INTO "table1" VALUES(NULL, 'B', 2, FALSE, '2022-07-20T00:00:00.000Z');`)
     })
     it(`Produces [INSERT INTO "table1"("col1") VALUES('A');]`, () => {
       const actual = sql.insertInto(table1, col1).values('A').getSQL()

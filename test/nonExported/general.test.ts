b0baa6ef9d75e550bff119754efb941a50746b78
Amer Harb <amer.harb@gmail.com>
2022-07-22 21:47:20 +0200

    test isTestBoolean and isTextNumber

--- test/nonExported/general.test.ts
+++ test/nonExported/general.test.ts
@@ -1,8 +1,8 @@
-import { Builder, e } from '../../src'
+import { Builder } from '../../src'
 
 // test non-exported Classes
 import { ReturningItemInfo } from '../../src/ReturningItemInfo'
-
+import { isTextBoolean, isTextNumber } from '../../src/models/types'
 import { database } from '../database'
 
 //Alias
@@ -16,6 +16,17 @@ describe('Import non-exported Classes', () => {
       const rii = new ReturningItemInfo(table1.c.col1, 'someAlias')
       const actual = sql.deleteFrom(table1).returning(rii).getSQL()
       expect(actual).toEqual(`DELETE FROM "table1" RETURNING "col1" AS "someAlias";`)
+    })
+  })
+  describe('Import: { isTextBoolean, isTextNumber }', () => {
+    it(`isTextBoolean return false for not string`, () => {
+      expect(isTextBoolean(123)).toEqual(false)
+    })
+    it(`isTextNumber return false for non-string`, () => {
+      expect(isTextNumber('abc')).toEqual(false)
+    })
+    it(`isTextNumber return false for non-string`, () => {
+      expect(isTextNumber(123)).toEqual(false)
     })
   })
 })

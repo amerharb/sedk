707d66c86e603851f4fac1781d061bf15736b42e
Amer Harb <amer.harb@gmail.com>
2022-09-30 08:33:19 +0200

    format

--- test/exported/error.test.ts
+++ test/exported/error.test.ts
@@ -281,28 +281,28 @@ describe('Throw desired Errors', () => {
 	})
 
 	it.each([
-		function actual1() {sql.selectAsteriskFrom(table1).limit(-1)},
-		function actual2() {sql.selectAsteriskFrom(table1).limit$(-1)},
-		function actual3() {sql.selectAsteriskFrom(table1).limit(NaN)},
-		function actual4() {sql.selectAsteriskFrom(table1).limit$(NaN)},
-		function actual5() {sql.selectAsteriskFrom(table1).limit(Number.POSITIVE_INFINITY)},
-		function actual6() {sql.selectAsteriskFrom(table1).limit$(Number.POSITIVE_INFINITY)},
-		function actual7() {sql.selectAsteriskFrom(table1).limit(Number.NEGATIVE_INFINITY)},
-		function actual8() {sql.selectAsteriskFrom(table1).limit$(Number.NEGATIVE_INFINITY)},
+		() => {sql.selectAsteriskFrom(table1).limit(-1)},
+		() => {sql.selectAsteriskFrom(table1).limit$(-1)},
+		() => {sql.selectAsteriskFrom(table1).limit(NaN)},
+		() => {sql.selectAsteriskFrom(table1).limit$(NaN)},
+		() => {sql.selectAsteriskFrom(table1).limit(Number.POSITIVE_INFINITY)},
+		() => {sql.selectAsteriskFrom(table1).limit$(Number.POSITIVE_INFINITY)},
+		() => {sql.selectAsteriskFrom(table1).limit(Number.NEGATIVE_INFINITY)},
+		() => {sql.selectAsteriskFrom(table1).limit$(Number.NEGATIVE_INFINITY)},
 	])('Throws error when LIMIT step has invalid value', (actual) => {
 		expect(actual).toThrow(InvalidLimitValueError)
 		expect(actual).toThrow(/^Invalid limit value: .*, value must be positive number.*/)
 	})
 
 	it.each([
-		function actual1() {sql.selectAsteriskFrom(table1).offset(-1)},
-		function actual2() {sql.selectAsteriskFrom(table1).offset$(-1)},
-		function actual3() {sql.selectAsteriskFrom(table1).offset(NaN)},
-		function actual4() {sql.selectAsteriskFrom(table1).offset$(NaN)},
-		function actual5() {sql.selectAsteriskFrom(table1).offset(Number.POSITIVE_INFINITY)},
-		function actual6() {sql.selectAsteriskFrom(table1).offset$(Number.POSITIVE_INFINITY)},
-		function actual7() {sql.selectAsteriskFrom(table1).offset(Number.NEGATIVE_INFINITY)},
-		function actual8() {sql.selectAsteriskFrom(table1).offset$(Number.NEGATIVE_INFINITY)},
+		() => {sql.selectAsteriskFrom(table1).offset(-1)},
+		() => {sql.selectAsteriskFrom(table1).offset$(-1)},
+		() => {sql.selectAsteriskFrom(table1).offset(NaN)},
+		() => {sql.selectAsteriskFrom(table1).offset$(NaN)},
+		() => {sql.selectAsteriskFrom(table1).offset(Number.POSITIVE_INFINITY)},
+		() => {sql.selectAsteriskFrom(table1).offset$(Number.POSITIVE_INFINITY)},
+		() => {sql.selectAsteriskFrom(table1).offset(Number.NEGATIVE_INFINITY)},
+		() => {sql.selectAsteriskFrom(table1).offset$(Number.NEGATIVE_INFINITY)},
 	])('Throws error when OFFSET step has invalid value', (actual) => {
 		expect(actual).toThrow(InvalidOffsetValueError)
 		expect(actual).toThrow(/^Invalid offset value: .*, value must be positive number$/)

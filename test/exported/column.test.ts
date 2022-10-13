a7edef00c160c0b3abc0be11af2dbd46ab858d3c
Amer Harb <amer.harb@gmail.com>
2022-10-13 14:53:17 +0200

    increase coverage of test of BooleanColumn

---
+++ test/exported/column.test.ts
@@ -0,0 +1,13 @@
+import { Builder } from 'src'
+
+import { ReturningItemInfo } from 'Non-Exported/ReturningItemInfo'
+import { database } from 'test/database'
+
+describe('BooleanColumn', () => {
+	const sql = new Builder(database, { throwErrorIfDeleteHasNoCondition: false })
+	afterEach(() => { sql.cleanUp() })
+	it(`getColumns returns array of one item of itself`, () => {
+		const actual = database.s.public.t.table1.c.col7.getColumns()
+		expect(actual).toEqual([database.s.public.t.table1.c.col7])
+	})
+})

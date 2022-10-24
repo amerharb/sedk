741ea4e50972642e573234deb6eaba0e5ab57d15
Amer Harb <amer.harb@gmail.com>
2022-10-24 21:07:22 +0200

    add unit test for DeleteStep

---
+++ test/nonExported/steps/delete-path/DeleteStep.test.ts
@@ -0,0 +1,10 @@
+import { DeleteStep, RootStep } from 'Non-Exported/steps'
+import { builderData } from 'test/nonExported/steps/builderData'
+
+describe('DeleteStep', () => {
+	const rootStep = new RootStep(builderData)
+	it('returns: [DELETE]', () => {
+		const actual = new DeleteStep(builderData, rootStep).getStepStatement()
+		expect(actual).toEqual('DELETE')
+	})
+})

97cbb946d7e68ef3437017295fe8a63f3778def2
Amer Harb <amer.harb@gmail.com>
2022-10-26 08:16:56 +0200

    unit test for ValueStep pass: 387 fail: 135

--- test/nonExported/steps/insert-path/IntoStep.test.ts
+++ test/nonExported/steps/insert-path/IntoStep.test.ts
@@ -4,6 +4,8 @@ import { builderData } from 'test/nonExported/steps/builderData'
 
 //Aliases
 const table1 = database.s.public.t.table1
+const col1 = table1.c.col1
+const col4 = table1.c.col4
 
 describe('IntoStep', () => {
 	const rootStep = new RootStep(builderData)
@@ -12,4 +14,8 @@ describe('IntoStep', () => {
 		expect(actual).toEqual('INTO "table1"')
 	})
 	it.todo('returns: [INTO "schema1"."table1"]')
+	it('returns: [INTO "table1"("col1", "col4")]', () => {
+		const actual = new IntoStep(builderData, rootStep, table1, [col1, col4]).getStepStatement()
+		expect(actual).toEqual('INTO "table1"("col1", "col4")')
+	})
 })
---
+++ test/nonExported/steps/insert-path/ValuesStep.test.ts
@@ -0,0 +1,13 @@
+import { RootStep, ValuesStep } from 'Non-Exported/steps'
+import { builderData } from 'test/nonExported/steps/builderData'
+
+describe('ValuesStep', () => {
+	const rootStep = new RootStep(builderData)
+	it(`returns: [VALUES(1, 'A')]`, () => {
+		const actual = new ValuesStep(builderData, rootStep, [1, 'A']).getStepStatement()
+		expect(actual).toEqual(`VALUES(1, 'A')`)
+	})
+	it.todo('returns: values with dates')
+	it.todo('returns: values DEFAULT')
+	it.todo('returns: values select')
+})

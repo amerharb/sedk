7d14921f9a26187006abd81a433d23f5c5167159
Amer Harb <amer.harb@gmail.com>
2022-11-04 13:32:34 +0100

    unit test for Table.ts

---
+++ test/unit/database/Table.test.ts
@@ -0,0 +1,17 @@
+import { database } from 'test/database'
+
+//Aliases
+const table1 = database.s.public.t.table1
+
+describe('Table', () => {
+	describe('class Table', () => {
+		// TODO: Add tests for Table
+	})
+	describe('class AliasedTable', () => {
+		const aliasedTable = table1.as('t1')
+		it('fqName/name', () => {
+			expect(aliasedTable.fqName).toEqual('"public"."table1"')
+			expect(aliasedTable.name).toEqual('table1')
+		})
+	})
+})

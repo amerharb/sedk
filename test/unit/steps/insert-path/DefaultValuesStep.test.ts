f8c17a7cc80e64a42ae5fe6df0430dc3d4f10748
Amer Harb <amer.harb@gmail.com>
2022-11-04 13:55:16 +0100

    add unit test for returning function in DefaultValuesStep class

--- test/unit/steps/insert-path/DefaultValuesStep.test.ts
+++ test/unit/steps/insert-path/DefaultValuesStep.test.ts
@@ -1,10 +1,19 @@
+import { ASTERISK } from 'Non-Exported/singletoneConstants'
 import { DefaultValuesStep, RootStep } from 'Non-Exported/steps'
 import { builderData } from 'test/unit/steps/builderData'
 
 describe('DefaultValuesStep', () => {
 	const rootStep = new RootStep(builderData)
-	it('returns: [DEFAULT VALUES]', () => {
-		const actual = new DefaultValuesStep(rootStep).getStepStatement()
-		expect(actual).toEqual('DEFAULT VALUES')
+	describe('getStepStatement()', () => {
+		it('returns: [DEFAULT VALUES]', () => {
+			const actual = new DefaultValuesStep(rootStep).getStepStatement()
+			expect(actual).toEqual('DEFAULT VALUES')
+		})
+	})
+	describe('returning()', () => {
+		it('returns: [RETURNING *]', () => {
+			const actual = new DefaultValuesStep(rootStep).returning(ASTERISK)
+			expect(actual.getStepStatement()).toEqual('RETURNING *')
+		})
 	})
 })

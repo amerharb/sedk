3feffb0969f7a9aab1bedc64a4df2860e9fe7c31
Amer Harb <amer.harb@gmail.com>
2022-11-04 18:39:48 +0100

    unit test for functions.ts

---
+++ test/unit/functions.test.ts
@@ -0,0 +1,23 @@
+import { Binder } from 'Non-Exported/binder'
+import { Condition, Expression } from 'Non-Exported/models'
+import { Artifacts } from 'Non-Exported/steps/BaseStep'
+import { ArithmeticOperator, ComparisonOperator, e } from 'src'
+import { builderData } from 'test/unit/steps/builderData'
+
+//Aliases
+const Equal = ComparisonOperator.Equal
+const ADD = ArithmeticOperator.ADD
+const emptyArtifacts: Artifacts = { tables: new Set(), columns: new Set() }
+
+describe('functions', () => {
+	describe('e()', () => {
+		it('returns Condition', () => {
+			expect(e(e(1), Equal, e(1))).toBeInstanceOf(Condition)
+		})
+		it('takes Binder', () => {
+			const actual = e(new Binder(1), ADD, 1)
+			expect(actual).toBeInstanceOf(Expression)
+			expect(actual.getStmt(builderData, emptyArtifacts)).toEqual('($1 + 1)')
+		})
+	})
+})

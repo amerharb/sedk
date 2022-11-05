c7123de8328650a9f9420c595134aae7a1fe07a3
Amer Harb <amer.harb@gmail.com>
2022-11-05 12:04:53 +0100

    testing throwing error for invalid  value in Operand

---
+++ test/unit/models/Operand.test.ts
@@ -0,0 +1,21 @@
+import { BinderStore } from 'Non-Exported/binder'
+import { Operand } from 'Non-Exported/models/Operand'
+import { builderData } from 'test/unit/steps/builderData'
+
+describe('Operand', () => {
+	describe('getStmtOfValue()', () => {
+		it('throws "Operand type of value: ? is not supported"', () => {
+			const arg = { notSupported: 'invalid' }
+			// @ts-ignore - for testing we are calling a private function
+			const actual = () => Operand.getStmtOfValue(
+				// @ts-ignore - for testing the arg is invalid
+				arg,
+				false,
+				builderData,
+				{ tables: new Set(), columns: new Set() },
+				new BinderStore(0),
+			)
+			expect(actual).toThrow(`Operand type of value: ${arg} is not supported`)
+		})
+	})
+})

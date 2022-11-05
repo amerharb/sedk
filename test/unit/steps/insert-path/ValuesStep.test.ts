b36140881a09629903d59497dcba3c2bbac56bb1
Amer Harb <amer.harb@gmail.com>
2022-11-05 10:21:29 +0100

    fix 2 todo unit test

--- test/unit/steps/insert-path/ValuesStep.test.ts
+++ test/unit/steps/insert-path/ValuesStep.test.ts
@@ -1,3 +1,4 @@
+import { DEFAULT } from 'Non-Exported/singletoneConstants'
 import { RootStep, ValuesStep } from 'Non-Exported/steps'
 import { builderData } from 'test/unit/steps/builderData'
 
@@ -7,7 +8,12 @@ describe('ValuesStep', () => {
 		const actual = new ValuesStep(rootStep, [1, 'A']).getStepStatement()
 		expect(actual).toEqual(`VALUES(1, 'A')`)
 	})
-	it.todo('returns: values with dates')
-	it.todo('returns: values DEFAULT')
-	it.todo('returns: values select')
+	it(`returns: [VALUES('1979-11-14T01:00:00.000Z', FALSE]`, () => {
+		const actual = new ValuesStep(rootStep, [new Date('1979-11-14 02:00:00'), false]).getStepStatement()
+		expect(actual).toEqual(`VALUES('1979-11-14T01:00:00.000Z', FALSE)`)
+	})
+	it(`returns: [VALUES(DEFAULT)]`, () => {
+		const actual = new ValuesStep(rootStep, [DEFAULT]).getStepStatement()
+		expect(actual).toEqual(`VALUES(DEFAULT)`)
+	})
 })

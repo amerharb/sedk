8ae3ad6364215457036b0587461b55bec05ded63
Amer Harb <amer.harb@gmail.com>
2022-11-05 07:46:04 +0100

    check cases of calling binder before sql

--- test/exported/binder.test.ts
+++ test/exported/binder.test.ts
@@ -1,8 +1,12 @@
-import { $, builder } from 'src'
+import { $, LogicalOperator, builder } from 'src'
 import { database } from 'test/database'
 
 //Alias
 const table1 = database.s.public.t.table1
+const col1 = table1.c.col1
+const col2 = table1.c.col2
+const col3 = table1.c.col3
+const AND = LogicalOperator.AND
 
 describe('Test binder with multi builders', () => {
 	const sql = builder(database)
@@ -52,5 +56,23 @@ describe('Test binder with multi builders', () => {
 		}
 		expect(actual.getSQL()).toEqual(expected.sql)
 		expect(actual.getBindValues()).toEqual(expected.values)
+	})
+
+	it(`Produces [['a', 'b', 'c']] without calling getSQL first`, () => {
+		const actual = sql
+			.selectAsteriskFrom(table1)
+			.where(col1.eq$('a'), AND, col2.eq$('b'))
+			.and(col3.eq$('c'))
+
+		const expected = ['a', 'b', 'c']
+
+		expect(actual.getBindValues()).toEqual(expected)
+
+		// result the same second time
+		expect(actual.getBindValues()).toEqual(expected)
+
+		// result the same after calling getSQL
+		actual.getSQL()
+		expect(actual.getBindValues()).toEqual(expected)
 	})
 })

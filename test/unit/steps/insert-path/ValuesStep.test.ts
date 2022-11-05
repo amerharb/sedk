44397d1c9e91666ef912a73e9511c80f74700ce5
Amer Harb <amer.harb@gmail.com>
2022-11-05 19:43:54 +0100

    fix for all timezone

--- test/unit/steps/insert-path/ValuesStep.test.ts
+++ test/unit/steps/insert-path/ValuesStep.test.ts
@@ -8,9 +8,9 @@ describe('ValuesStep', () => {
 		const actual = new ValuesStep(rootStep, [1, 'A']).getStepStatement()
 		expect(actual).toEqual(`VALUES(1, 'A')`)
 	})
-	it(`returns: [VALUES('1979-11-14T01:00:00.000Z', FALSE]`, () => {
-		const actual = new ValuesStep(rootStep, [new Date('1979-11-14 02:00:00'), false]).getStepStatement()
-		expect(actual).toEqual(`VALUES('1979-11-14T01:00:00.000Z', FALSE)`)
+	it(`returns: [VALUES('1979-11-14T02:00:00.000Z', FALSE]`, () => {
+		const actual = new ValuesStep(rootStep, [new Date('1979-11-14 02:00:00Z'), false]).getStepStatement()
+		expect(actual).toEqual(`VALUES('1979-11-14T02:00:00.000Z', FALSE)`)
 	})
 	it(`returns: [VALUES(DEFAULT)]`, () => {
 		const actual = new ValuesStep(rootStep, [DEFAULT]).getStepStatement()

c3c54b9b9a71e8e76df2ce846b3b3f76706d6c03
Amer Harb <amer.harb@gmail.com>
2022-11-06 06:01:58 +0100

    start unit test for AfterFromStep

---
+++ test/unit/steps/select-path/AfterFromStep.test.ts
@@ -0,0 +1,16 @@
+import { CrossJoinStep, RootStep } from 'Non-Exported/steps'
+import { builderData } from 'test/unit/steps/builderData'
+import { database } from 'test/database'
+
+//Aliases
+const table1 = database.s.public.t.table1
+
+describe('AfterFromStep', () => {
+	const rootStep = new RootStep(builderData)
+	describe('CrossJoinStep', () => {
+		it(`returns: [CROSS JOIN "table1" AS "t2"]`, () => {
+			const actual = new CrossJoinStep(rootStep, table1.as('t2')).getStepStatement()
+			expect(actual).toEqual('CROSS JOIN "table1" AS "t2"')
+		})
+	})
+})

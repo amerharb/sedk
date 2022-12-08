8fc15e9fedf14e1273d7f9a268e07760fbe06ac8
Amer Harb <amer.harb@gmail.com>
2022-12-08 13:54:42 +0100

    add more unit test

--- test/unit/util.test.ts
+++ test/unit/util.test.ts
@@ -10,6 +10,22 @@ describe('util', () => {
 			const actual = getMinOneArray(['a', 'b'])
 			expect(actual).toStrictEqual(['a', 'b'])
 		})
+		it(`returns: ['a', 'b', 'c']`, () => {
+			const actual = getMinOneArray(['a', 'b', 'c'])
+			expect(actual).toStrictEqual(['a', 'b', 'c'])
+		})
+		it(`returns: [1]`, () => {
+			const actual = getMinOneArray([1])
+			expect(actual).toStrictEqual([1])
+		})
+		it(`returns: [1, 2]`, () => {
+			const actual = getMinOneArray([1, 2])
+			expect(actual).toStrictEqual([1, 2])
+		})
+		it(`returns: [1, 2, 3]`, () => {
+			const actual = getMinOneArray([1, 2, 3])
+			expect(actual).toStrictEqual([1, 2, 3])
+		})
 		it('throws: "Array must have at least one element"', () => {
 			const actual = () => getMinOneArray([])
 			expect(actual).toThrow('Array must have at least one element')

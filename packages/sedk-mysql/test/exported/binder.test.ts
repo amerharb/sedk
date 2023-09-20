a939f1bd0fa3951aa3b7cb53f52d940f3f5e01f6
Amer Harb <amer.harb@gmail.com>
2023-09-20 08:48:33 +0200

    fix binders

--- packages/sedk-mysql/test/exported/binder.test.ts
+++ packages/sedk-mysql/test/exported/binder.test.ts
@@ -13,7 +13,7 @@ describe('Test binder with multi builders', () => {
 	const sql1 = builder(database)
 	const sql2 = builder(database)
 
-	it('Produces [SELECT $1 FROM "table1";] 2 binds at the same time', () => {
+	it('Produces [SELECT ? FROM `table1`;] 2 binds at the same time', () => {
 		const actual1 = sql1
 			.select($(1))
 			.from(table1)
@@ -21,36 +21,36 @@ describe('Test binder with multi builders', () => {
 			.select($(2))
 			.from(table1)
 
-		expect(actual1.getSQL()).toEqual('SELECT $1 FROM "table1";')
+		expect(actual1.getSQL()).toEqual('SELECT ? FROM `table1`;')
 		expect(actual1.getBindValues()).toEqual([1])
-		expect(actual2.getSQL()).toEqual('SELECT $1 FROM "table1";')
+		expect(actual2.getSQL()).toEqual('SELECT ? FROM `table1`;')
 		expect(actual2.getBindValues()).toEqual([2])
 	})
-	it(`Produces [SELECT $1 FROM "table1";]`, () => {
+	it('Produces [SELECT ? FROM `table1`;]', () => {
 		const actual = sql
 			.select($(5))
 			.from(table1)
 
 		const expected = {
-			sql: `SELECT $1 FROM "table1";`,
+			sql: 'SELECT ? FROM `table1`;',
 			values: [5],
 		}
 		expect(actual.getSQL()).toEqual(expected.sql)
 		expect(actual.getBindValues()).toEqual(expected.values)
 	})
-	it(`Produces [SELECT $1, $2, $3, $4 FROM "table1";]`, () => {
+	it('Produces [SELECT ?, ?, ?, ? FROM `table1`;]', () => {
 		const actual = sql
 			.select($(null), $(true), $(1), $(`a`))
 			.from(table1)
 
 		const expected = {
-			sql: `SELECT $1, $2, $3, $4 FROM "table1";`,
+			sql: 'SELECT ?, ?, ?, ? FROM `table1`;',
 			values: [null, true, 1, `a`],
 		}
 		expect(actual.getSQL()).toEqual(expected.sql)
 		expect(actual.getBindValues()).toEqual(expected.values)
 	})
-	it(`Produces [['a', 'b', 'c']] without calling getSQL first`, () => {
+	it("Produces [['a', 'b', 'c']] without calling getSQL first", () => {
 		const select = sql.selectAsteriskFrom(table1)
 		const getStmtSelect = jest.spyOn(select, 'getStepStatement')
 		const where = select.where(col1.eq$('a'), AND, col2.eq$('b'))

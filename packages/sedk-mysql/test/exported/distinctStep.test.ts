ad800f45f34b386769d7dc969c7ea117a406b35e
Amer Harb <amer.harb@gmail.com>
2023-09-21 11:08:59 +0200

    fiz unit test fail: 132

--- packages/sedk-mysql/test/exported/distinctStep.test.ts
+++ packages/sedk-mysql/test/exported/distinctStep.test.ts
@@ -14,66 +14,66 @@ describe('test orderBy Step', () => {
 	const sql = builder(database)
 
 	/** TODO: check mySQL if it is ok to have FROM without any columns */
-	it('Produces [SELECT ALL FROM "table1";]', () => {
+	it('Produces [SELECT ALL FROM `table1`;]', () => {
 		const actual = sql
 			.selectAll()
 			.from(table)
 			.getSQL()
 
-		expect(actual).toEqual('SELECT ALL FROM "table1";')
+		expect(actual).toEqual('SELECT ALL FROM `table1`;')
 	})
 
-	it('Produces [SELECT DISTINCT "col1", "col2" FROM "table1";]', () => {
+	it('Produces [SELECT DISTINCT `col1`, `col2` FROM `table1`;]', () => {
 		const actual = sql
 			.selectDistinct(col1, col2)
 			.from(table)
 			.getSQL()
 
-		expect(actual).toEqual('SELECT DISTINCT "col1", "col2" FROM "table1";')
+		expect(actual).toEqual('SELECT DISTINCT `col1`, `col2` FROM `table1`;')
 	})
 
-	it('Produces [SELECT ALL "col1", "col2" FROM "table1";]', () => {
+	it('Produces [SELECT ALL `col1`, `col2` FROM `table1`;]', () => {
 		const actual = sql
 			.selectAll(col1, col2)
 			.from(table)
 			.getSQL()
 
-		expect(actual).toEqual('SELECT ALL "col1", "col2" FROM "table1";')
+		expect(actual).toEqual('SELECT ALL `col1`, `col2` FROM `table1`;')
 	})
 
-	it('Produces [SELECT DISTINCT "col1", "col2" FROM "table1";] using select first param', () => {
+	it('Produces [SELECT DISTINCT `col1`, `col2` FROM `table1`;] using select first param', () => {
 		const actual = sql
 			.select(DISTINCT, col1, col2)
 			.from(table)
 			.getSQL()
 
-		expect(actual).toEqual('SELECT DISTINCT "col1", "col2" FROM "table1";')
+		expect(actual).toEqual('SELECT DISTINCT `col1`, `col2` FROM `table1`;')
 	})
 
-	it('Produces [SELECT ALL "col1", "col2" FROM "table1";] using select first param', () => {
+	it('Produces [SELECT ALL `col1`, `col2` FROM `table1`;] using select first param', () => {
 		const actual = sql
 			.select(ALL, col1, col2)
 			.from(table)
 			.getSQL()
 
-		expect(actual).toEqual('SELECT ALL "col1", "col2" FROM "table1";')
+		expect(actual).toEqual('SELECT ALL `col1`, `col2` FROM `table1`;')
 	})
 
-	it('Produces [SELECT ALL FROM "table1";] only ALL is valid (as param)', () => {
+	it('Produces [SELECT ALL FROM `table1`;] only ALL is valid (as param)', () => {
 		const actual = sql
 			.select(ALL)
 			.from(table)
 			.getSQL()
 
-		expect(actual).toEqual('SELECT ALL FROM "table1";')
+		expect(actual).toEqual('SELECT ALL FROM `table1`;')
 	})
 
-	it('Produces [SELECT ALL FROM "table1";] only ALL is valid (using selectAll())', () => {
+	it('Produces [SELECT ALL FROM `table1`;] only ALL is valid (using selectAll())', () => {
 		const actual = sql
 			.selectAll()
 			.from(table)
 			.getSQL()
 
-		expect(actual).toEqual('SELECT ALL FROM "table1";')
+		expect(actual).toEqual('SELECT ALL FROM `table1`;')
 	})
 })

ff41aad527d68307418d51c140b75e83b5596d51
Amer Harb <amer.harb@gmail.com>
2023-09-24 12:19:20 +0200

    lint mysql-ts example

--- examples/sedk-mysql-ts-example/example1.ts
+++ examples/sedk-mysql-ts-example/example1.ts
@@ -1,24 +1,24 @@
 import './readme-example.js'
-import * as sedk from 'sedk-mysql';
+import * as sedk from 'sedk-mysql'
 
 // Schema definition (practically this should be defined in one separate file for the whole project)
 export const database = new sedk.Database({
-  version: 1,
-  schemas: {
-    public: new sedk.Schema({
-      name: 'public',
-      tables: {
-        Employee: new sedk.Table({
-          name: 'Employee',
-          columns: {
-            name: new sedk.TextColumn({ name: 'name' }),
-            salary: new sedk.NumberColumn({ name: 'salary' }),
-            isManager: new sedk.BooleanColumn({ name: 'isManager' }),
-          },
-        }),
-      },
-    }),
-  },
+	version: 1,
+	schemas: {
+		public: new sedk.Schema({
+			name: 'public',
+			tables: {
+				Employee: new sedk.Table({
+					name: 'Employee',
+					columns: {
+						name: new sedk.TextColumn({ name: 'name' }),
+						salary: new sedk.NumberColumn({ name: 'salary' }),
+						isManager: new sedk.BooleanColumn({ name: 'isManager' }),
+					},
+				}),
+			},
+		}),
+	},
 })
 
 // Aliases
@@ -43,7 +43,7 @@ console.assert(stmt2 === expt2, 'stmt2 is not as expected')
 
 const binderExample = sql.select(name, salary).from(Employee).where(name.eq$('John'), AND, salary.gt$(1500))
 console.log(binderExample.getSQL())
-const expt3 = "SELECT `name`, `salary` FROM `Employee` WHERE ( `name` = ? AND `salary` > ? );"
+const expt3 = 'SELECT `name`, `salary` FROM `Employee` WHERE ( `name` = ? AND `salary` > ? );'
 console.assert(binderExample.getSQL() === expt3, 'binderExample.getSQL() is not as expected')
 console.log(binderExample.getBindValues())
 const expt3Arr = [ 'John', 1500 ]
@@ -51,7 +51,7 @@ console.assert(JSON.stringify(binderExample.getBindValues()) === JSON.stringify(
 
 const bind4 = sql.selectAsteriskFrom(Employee).where(name.eq$('John'), AND, salary.gt$(1500))
 console.log(bind4.getSQL())
-const expt4 = "SELECT * FROM `Employee` WHERE ( `name` = ? AND `salary` > ? );"
+const expt4 = 'SELECT * FROM `Employee` WHERE ( `name` = ? AND `salary` > ? );'
 console.assert(bind4.getSQL() === expt4, 'bind4.getSQL() is not as expected')
 console.log(bind4.getBindValues())
 const expt4Arr = [ 'John', 1500 ]
@@ -59,7 +59,7 @@ console.assert(JSON.stringify(bind4.getBindValues()) === JSON.stringify(expt4Arr
 
 const bind5 = sql.deleteFrom(Employee).where(name.eq$('John'), AND, salary.gt$(1500))
 console.log(bind5.getSQL())
-const expt5 = "DELETE FROM `Employee` WHERE ( `name` = ? AND `salary` > ? );"
+const expt5 = 'DELETE FROM `Employee` WHERE ( `name` = ? AND `salary` > ? );'
 console.assert(bind5.getSQL() === expt5, 'bind5.getSQL() is not as expected')
 console.log(bind5.getBindValues())
 const expt5Arr = [ 'John', 1500 ]

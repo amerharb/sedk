d4b9c166557eb96aac84722552ecf7b1f7735929
Amer Harb <amer.harb@gmail.com>
2023-11-16 02:16:56 +0100

    add Join step

---
+++ packages/sedk-mysql/src/steps/select-path/BaseJoinStep.ts
@@ -0,0 +1,52 @@
+import { FromItem } from './SelectFromStep'
+import { Condition } from '../../models'
+import { OnStep } from './AfterFromStep'
+import { Table } from '../../database'
+import { Artifacts, BaseStep } from '../BaseStep'
+
+abstract class BaseJoinStep extends BaseStep {
+	protected constructor(
+		private readonly joinType: 'JOIN'|'LEFT JOIN'|'RIGHT JOIN'|'INNER JOIN',
+		prevStep: BaseStep,
+		private readonly fromItem: FromItem,
+	) {
+		super(prevStep)
+	}
+
+	public on(condition: Condition): OnStep {
+		return new OnStep(this, condition)
+	}
+
+	getStepStatement(artifacts: Artifacts = { tables: new Set(), columns: new Set() }): string {
+		return `${this.joinType} ${this.fromItem.getStmt(this.data, artifacts)}`
+	}
+
+	getStepArtifacts(): Artifacts {
+		const table = this.fromItem instanceof Table ? this.fromItem : this.fromItem.table
+		return { tables: new Set([table]), columns: new Set() }
+	}
+}
+
+export class JoinStep extends BaseJoinStep {
+	public constructor(prevStep: BaseStep, fromItem: FromItem) {
+		super('JOIN', prevStep, fromItem)
+	}
+}
+
+export class LeftJoinStep extends BaseJoinStep {
+	public constructor(prevStep: BaseStep, fromItem: FromItem) {
+		super('LEFT JOIN', prevStep, fromItem)
+	}
+}
+
+export class RightJoinStep extends BaseJoinStep {
+	public constructor(prevStep: BaseStep, fromItem: FromItem) {
+		super('RIGHT JOIN', prevStep, fromItem)
+	}
+}
+
+export class InnerJoinStep extends BaseJoinStep {
+	public constructor(prevStep: BaseStep, fromItem: FromItem) {
+		super('INNER JOIN', prevStep, fromItem)
+	}
+}

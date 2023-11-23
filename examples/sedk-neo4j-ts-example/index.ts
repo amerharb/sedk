2f833949c68a2713fc55174bb862d744c53b0e32
Amer Harb <amer.harb@gmail.com>
2023-11-23 22:28:04 +0100

    update all ts examples

--- examples/sedk-neo4j-ts-example/index.ts
+++ examples/sedk-neo4j-ts-example/index.ts
@@ -1,17 +1,23 @@
 import { log } from './util.js'
 
-const importFunctionsList = [
-	async () => await import('./readme-example.js'),
+const examples = [
+	'./readme-example.js',
 ]
 
 async function main() {
-	for (const fn of importFunctionsList) {
-		log(`📦 run ${fn.name}`)
-		await fn()
+	const functions = examples.map((filename) => ({
+		filename,
+		execFile: async () => await import(filename)
+	}))
+	for (const fn of functions) {
+		log(`📦 run ${fn.filename}`)
+		await fn.execFile()
+		log('')
 	}
 }
 
 log('🚀 Starting sedk-neo4j-ts-example ...')
+log('')
 main().then(() => {
 	log('👋 End of sedk-neo4j-ts-example')
 })

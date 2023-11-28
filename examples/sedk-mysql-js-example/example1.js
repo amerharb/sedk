05fe6b8c93c749d1e8b7294bf5b7c10be7dd3ccb
Amer Harb <amer.harb@gmail.com>
2023-11-28 20:47:32 +0100

    update assert msg

--- examples/sedk-mysql-js-example/example1.js
+++ examples/sedk-mysql-js-example/example1.js
@@ -32,34 +32,34 @@ const sql = sedk.builder(database)
 const stmt1 = sql.select(name, salary).from(Employee).where(name.eq('John'), AND, salary.gt(1500)).getSQL()
 console.log(stmt1)
 const expt1 = "SELECT `name`, `salary` FROM `Employee` WHERE ( `name` = 'John' AND `salary` > 1500 );"
-console.assert(stmt1 === expt1, 'stmt1 is not as expected')
+console.assert(stmt1 === expt1, '❌ stmt1 is not as expected')
 
 // Also it can be written as
 const stmt2 = sql.select(name, salary).from(Employee).where(name.eq('John')).and(salary.gt(1500)).getSQL()
 console.log(stmt2)
 const expt2 = "SELECT `name`, `salary` FROM `Employee` WHERE `name` = 'John' AND `salary` > 1500;"
-console.assert(stmt2 === expt2, 'stmt2 is not as expected')
+console.assert(stmt2 === expt2, '❌ stmt2 is not as expected')
 
 const binderExample = sql.select(name, salary).from(Employee).where(name.eq$('John'), AND, salary.gt$(1500))
 console.log(binderExample.getSQL())
 const expt3 = "SELECT `name`, `salary` FROM `Employee` WHERE ( `name` = ? AND `salary` > ? );"
-console.assert(binderExample.getSQL() === expt3, 'binderExample.getSQL() is not as expected')
+console.assert(binderExample.getSQL() === expt3, '❌ binderExample.getSQL() is not as expected')
 console.log(binderExample.getBindValues())
 const expt3Arr = ['John', 1500]
-console.assert(JSON.stringify(binderExample.getBindValues()) === JSON.stringify(expt3Arr), 'binderExample.getBindValues() is not as expected')
+console.assert(JSON.stringify(binderExample.getBindValues()) === JSON.stringify(expt3Arr), '❌ binderExample.getBindValues() is not as expected')
 
 const bind4 = sql.selectAsteriskFrom(Employee).where(name.eq$('John'), AND, salary.gt$(1500))
 console.log(bind4.getSQL())
 const expt4 = "SELECT * FROM `Employee` WHERE ( `name` = ? AND `salary` > ? );"
-console.assert(bind4.getSQL() === expt4, 'bind4.getSQL() is not as expected')
+console.assert(bind4.getSQL() === expt4, '❌ bind4.getSQL() is not as expected')
 console.log(bind4.getBindValues())
 const expt4Arr = ['John', 1500]
-console.assert(JSON.stringify(bind4.getBindValues()) === JSON.stringify(expt4Arr), 'bind4.getBindValues() is not as expected')
+console.assert(JSON.stringify(bind4.getBindValues()) === JSON.stringify(expt4Arr), '❌ bind4.getBindValues() is not as expected')
 
 const bind5 = sql.deleteFrom(Employee).where(name.eq$('John'), AND, salary.gt$(1500))
 console.log(bind5.getSQL())
 const expt5 = "DELETE FROM `Employee` WHERE ( `name` = ? AND `salary` > ? );"
-console.assert(bind5.getSQL() === expt5, 'bind5.getSQL() is not as expected')
+console.assert(bind5.getSQL() === expt5, '❌ bind5.getSQL() is not as expected')
 console.log(bind5.getBindValues())
 const expt5Arr = ['John', 1500]
-console.assert(JSON.stringify(bind5.getBindValues()) === JSON.stringify(expt5Arr), 'bind5.getBindValues() is not as expected')
+console.assert(JSON.stringify(bind5.getBindValues()) === JSON.stringify(expt5Arr), '❌ bind5.getBindValues() is not as expected')
--- examples/sedk-neo4j-js-example/readme-example.js
+++ examples/sedk-neo4j-js-example/readme-example.js
@@ -19,14 +19,14 @@ const cypher = sedk.builder()
 const stmt1 = cypher.match(n, Person).return(n).getCypher()
 console.log(stmt1)
 const expt1 = 'MATCH (`n`:`Person`) RETURN `n`'
-console.assert(stmt1 === expt1, 'stmt1 is not as expected')
+console.assert(stmt1 === expt1, '❌ stmt1 is not as expected')
 
 const stmt2 = cypher.match(n, Person).return(ASTERISK).getCypher()
 console.log(stmt2)
 const expt2 = 'MATCH (`n`:`Person`) RETURN *'
-console.assert(stmt2 === expt2, 'stmt2 is not as expected')
+console.assert(stmt2 === expt2, '❌ stmt2 is not as expected')
 
 const stmt3 = cypher.match(n, Person, Student).return(ASTERISK).getCypher()
 console.log(stmt3)
 const expt3 = 'MATCH (`n`:`Person`:`Student`) RETURN *'
-console.assert(stmt3 === expt3, 'stmt3 is not as expected')
+console.assert(stmt3 === expt3, '❌ stmt3 is not as expected')

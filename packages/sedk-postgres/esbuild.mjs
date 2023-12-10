d1f466f876038e11ce64dc82b643386af973ed95
Amer Harb <amer.harb@gmail.com>
2023-12-10 17:06:52 +0100

    use esbuild in sedk-postgres

---
+++ packages/sedk-postgres/esbuild.mjs
@@ -0,0 +1,17 @@
+'use strict';
+import esbuild from 'esbuild';
+import { transformExtPlugin } from '@gjsify/esbuild-plugin-transform-ext';
+
+async function main() {
+	await esbuild.build({
+		plugins: [transformExtPlugin({ outExtension: {'.ts': '.js'}})],
+		entryPoints: ['./src/**/*.ts'],
+		outdir: './dist/src/',
+		bundle: false,
+		format: 'esm',
+	});
+}
+
+main()
+ 	.then(() => console.log('Build complete'))
+	.catch(() => process.exit(1));
